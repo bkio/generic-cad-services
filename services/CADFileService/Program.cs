ac1bf637c26183528f317724b4e5062f3864257c
Berkan Uslu <berkanuslu@gmail.com>
2021-06-03 02:39:16 +0200

    UPDATED RootPath implementation by using DEPLOYMENT_BUILD_NUMBER instead of using DEPLOYMENT_BRANCH_NAME for non-master and non-development branches.
    ADDED VirtualMachineService.RunCommand implementation after returning onComplete action from VirtualMachineService.StartInstances function.
    UPDATED Some message contents.
    UPDATED Submodule which contains RunCommand implementation.

--- services/CADFileService/Program.cs
+++ services/CADFileService/Program.cs
@@ -81,7 +81,7 @@ namespace CADFileService
             var RootPath = "/";
             if (ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] != "master" && ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] != "development")
             {
-                RootPath = "/" + ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] + "/";
+                RootPath = "/" + ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BUILD_NUMBER"] + "/";
             }
 
             CommonData.MemoryQueryParameters = new BMemoryQueryParameters()
